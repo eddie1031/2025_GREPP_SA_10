db42c8459747d44aca55fd338c2e344e95821fb0
eddie1031 <archaco123@gmail.com>
2025-11-21 17:51:47 +0900

    communication

--- src/main/java/com/example/demo/services/AiService.java
+++ src/main/java/com/example/demo/services/AiService.java
@@ -3,6 +3,8 @@ package com.example.demo.services;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,7 +18,11 @@ public class AiService {
     }
 
     public ChatResponse generateAnswer(String question) {
-        return client.prompt(question).call().chatResponse();
+
+        OpenAiChatOptions options = new OpenAiChatOptions();
+        options.setModel("llama-3.1-8b-instant");
+
+        return client.prompt(new Prompt(question, options)).call().chatResponse();
     }
 
 }
